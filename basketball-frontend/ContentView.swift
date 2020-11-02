eabd6973f945bee20db2b7d4bb2c484291183b63
mcruz1@andrew.cmu.edu <mcruz1@andrew.cmu.edu>
2020-11-02 17:49:26 -0500

    Initial Commit

---
+++ basketball-frontend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basketball-frontend
+//
+//  Created by Matthew Cruz on 11/2/20.
+//  Copyright © 2020 Matthew Cruz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ basketball-frontendTests/basketball_frontendTests.swift
@@ -0,0 +1,34 @@
+//
+//  basketball_frontendTests.swift
+//  basketball-frontendTests
+//
+//  Created by Matthew Cruz on 11/2/20.
+//  Copyright © 2020 Matthew Cruz. All rights reserved.
+//
+
+import XCTest
+@testable import basketball_frontend
+
+class basketball_frontendTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
