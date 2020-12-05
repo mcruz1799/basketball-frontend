1f52730adb4868230e880e7543b46bea9d4b8829
mcruz1@andrew.cmu.edu <mcruz1@andrew.cmu.edu>
2020-12-05 02:44:35 -0500

    changed colors of the tab bar

--- basketball-frontend/Views/AppView.swift
+++ basketball-frontend/Views/AppView.swift
@@ -13,7 +13,10 @@ struct AppView: View {
   @State var creatingGame: Bool = false
   
   init() {
-    UITabBar.appearance().backgroundColor = UIColor.orange
+		UITabBar.appearance().isTranslucent = false
+		UITabBar.appearance().barTintColor = UIColor(named: "tabBarColor")
+		UITabBar.appearance().unselectedItemTintColor = UIColor(named: "tabBarIconColor")
+	
   }
   var body: some View {
     
@@ -21,62 +24,27 @@ struct AppView: View {
       HomeView(viewModel: viewModel)
         .tabItem{
           Image(systemName: "house.fill")
-            .font(.system(size: 25))
-            .imageScale(.large)
-          Text("Home")
+						.font(.system(size: 50))
+
         }
       CreateView(viewModel: viewModel)
         .tabItem{
           Image(systemName: "plus.circle.fill")
-            .font(.system(size: 36))
-            .imageScale(.large)
-          Text("Create Game")
-//            .onTapGesture {
-//              self.creatingGame = true
-//            }
+					
         }
       ProfileView(user: $viewModel.user, favorites: $viewModel.favorites, viewModel: viewModel)
         .tabItem{
           Image(systemName: "person.circle")
-            .font(.system(size: 25))
-            .imageScale(.large)
-          Text("Profile")
+						.font(.system(size: 2000))
+
+
         }
-    }.onAppear { self.viewModel.fetchData() }
-//    .sheet(isPresented: $creatingGame) {
-//      CreateFormView(viewModel: viewModel, creatingGame: $creatingGame)  
-//    }
+		}
+		.onAppear { self.viewModel.fetchData() }
   }
 }
 
-//
-//extension UIColor {
-//    public convenience init?(hex: String) {
-//        let r, g, b, a: CGFloat
-//
-//        if hex.hasPrefix("#") {
-//            let start = hex.index(hex.startIndex, offsetBy: 1)
-//            let hexColor = String(hex[start...])
-//
-//            if hexColor.count == 8 {
-//                let scanner = Scanner(string: hexColor)
-//                var hexNumber: UInt64 = 0
-//
-//                if scanner.scanHexInt64(&hexNumber) {
-//                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-//                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-//                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-//                    a = CGFloat(hexNumber & 0x000000ff) / 255
-//
-//                    self.init(red: r, green: g, blue: b, alpha: a)
-//                    return
-//                }
-//            }
-//        }
-//
-//        return nil
-//    }
-//}
+
 struct AppView_Previews: PreviewProvider {
   static var previews: some View {
     AppView()
