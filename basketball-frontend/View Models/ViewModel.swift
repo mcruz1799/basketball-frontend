49637b8a31d89e7e8fc9891a1d0bcd075abf0556
Jeffrey Igims <jigims@andrew.cmu.edu>
2020-12-05 16:29:48 -0500

    Project builds successfully

--- basketball-frontend/View Models/ViewModel.swift
+++ basketball-frontend/View Models/ViewModel.swift
@@ -24,7 +24,7 @@ class ViewModel: ObservableObject {
   @Published var invited: [Users] = [Users]()
   @Published var maybe: [Users] = [Users]()
   @Published var going: [Users] = [Users]()
-	
+  
   @Published var gamePlayers: Set<Int> = Set()
   
   @Published var userLocation = Location()
@@ -34,7 +34,7 @@ class ViewModel: ObservableObject {
   @Published var alert: Alert?
   @Published var showAlert: Bool = false
   
-	init () {}
+  init () {}
   
   //
   // USER FUNCTIONS
@@ -48,7 +48,7 @@ class ViewModel: ObservableObject {
     self.currentScreen = "login-splash"
     let credentials: String = username + ":" + password
     let encodedCredentials: String = Data(credentials.utf8).base64EncodedString()
-
+    
     let headers: HTTPHeaders = [
       "Authorization": "Basic " + encodedCredentials
     ]
@@ -56,25 +56,25 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/token/", headers: headers)
       .validate()
       .responseDecodable {
-      ( response: AFDataResponse<UserLogin> ) in
+        ( response: AFDataResponse<UserLogin> ) in
         switch response.result {
-          case .success:
-            if let value: UserLogin = response.value {
-              let token = value.api_key
-              self.userId = value.id
-              self.createAuthHeader(token: token)
-              self.refreshCurrentUser()
-              self.getGames()
-              self.currentScreen = "app"
-            }
-          case .failure:
-            self.currentScreen = "login"
-            self.alert = self.createAlert(title: "Invalid Login",
-                                          message: "The username or password you entered was invalid",
-                                          button: "Got it")
-            self.showAlert = true
+        case .success:
+          if let value: UserLogin = response.value {
+            let token = value.api_key
+            self.userId = value.id
+            self.createAuthHeader(token: token)
+            self.refreshCurrentUser()
+            self.getGames()
+            self.currentScreen = "app"
+          }
+        case .failure:
+          self.currentScreen = "login"
+          self.alert = self.createAlert(title: "Invalid Login",
+                                        message: "The username or password you entered was invalid",
+                                        button: "Got it")
+          self.showAlert = true
         }
-    }
+      }
   }
   
   //  refresh the current user by updating self.user
@@ -135,20 +135,20 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/create_user/", method: .post, parameters: params)
       .validate()
       .responseDecodable {
-      ( response: AFDataResponse<APIData<User>> ) in
+        ( response: AFDataResponse<APIData<User>> ) in
         switch response.result {
-          case .success:
-            if let value: APIData<User> = response.value {
-              user = value.data
-              self.login(username: username, password: password)
-            }
-          case .failure:
-            self.alert = self.createAlert(title: "Invalid Profile Information",
-                                          message: "The profile information you entered was invalid, please check your inputs and try again",
-                                          button: "Got it")
-            self.showAlert = true
+        case .success:
+          if let value: APIData<User> = response.value {
+            user = value.data
+            self.login(username: username, password: password)
+          }
+        case .failure:
+          self.alert = self.createAlert(title: "Invalid Profile Information",
+                                        message: "The profile information you entered was invalid, please check your inputs and try again",
+                                        button: "Got it")
+          self.showAlert = true
         }
-    }
+      }
     return user
   }
   
@@ -174,20 +174,20 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/users/" + String(self.user!.id), method: .patch, parameters: params, headers: self.headers!)
       .validate()
       .responseDecodable {
-      ( response: AFDataResponse<APIData<User>> ) in
+        ( response: AFDataResponse<APIData<User>> ) in
         switch response.result {
-          case .success:
-            if let value: APIData<User> = response.value {
-              self.user = value.data
-              self.refreshCurrentUser()
-            }
-          case .failure:
-            self.alert = self.createAlert(title: "Invalid Profile Information",
-                                          message: "The edited profile information you entered was invalid, please check your inputs and try again",
-                                          button: "Try again")
-            self.showAlert = true
+        case .success:
+          if let value: APIData<User> = response.value {
+            self.user = value.data
+            self.refreshCurrentUser()
+          }
+        case .failure:
+          self.alert = self.createAlert(title: "Invalid Profile Information",
+                                        message: "The edited profile information you entered was invalid, please check your inputs and try again",
+                                        button: "Try again")
+          self.showAlert = true
         }
-    }
+      }
   }
   
   //
@@ -301,34 +301,34 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/favorites", method: .post, parameters: params, headers: self.headers!)
       .validate(statusCode: 200..<300)
       .responseDecodable {
-      ( response: AFDataResponse<APIData<Favorite>> ) in
+        ( response: AFDataResponse<APIData<Favorite>> ) in
         switch response.result {
-          case .success:
-            if let value: APIData<Favorite> = response.value {
-              self.refreshCurrentUser()
-              favorite = value.data
-            }
-          case .failure:
-            print(response.result)
-            self.alert = Alert(title: Text("Favorite Failed"),
-                               message: Text("Failed to favorite this user, please try again"),
-                               primaryButton: .default(
-                                 Text("Try Again"),
-                               action: {
-                                 self.favorite(favoriterId: favoriterId, favoriteeId: favoriteeId)
-                               }
-                               ),
-                               secondaryButton: .default(
-                                 Text("Close"),
-                                 action: {
-                                   self.showAlert = false
-                                   self.alert = nil
-                                 }
-                           )
-                         )
-            self.showAlert = true
+        case .success:
+          if let value: APIData<Favorite> = response.value {
+            self.refreshCurrentUser()
+            favorite = value.data
           }
+        case .failure:
+          print(response.result)
+          self.alert = Alert(title: Text("Favorite Failed"),
+                             message: Text("Failed to favorite this user, please try again"),
+                             primaryButton: .default(
+                              Text("Try Again"),
+                              action: {
+                                self.favorite(favoriterId: favoriterId, favoriteeId: favoriteeId)
+                              }
+                             ),
+                             secondaryButton: .default(
+                              Text("Close"),
+                              action: {
+                                self.showAlert = false
+                                self.alert = nil
+                              }
+                             )
+          )
+          self.showAlert = true
         }
+      }
     return favorite
   }
   
@@ -365,32 +365,32 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/favorites/" + String(id), method: .delete, parameters: nil, headers: self.headers!)
       .validate()
       .responseDecodable {
-      ( response: AFDataResponse<APIData<Favorite>> ) in
+        ( response: AFDataResponse<APIData<Favorite>> ) in
         switch response.result {
-          case .success:
-            if let _: APIData<Favorite> = response.value {
-              self.refreshCurrentUser()
-            }
-          case .failure:
-            self.alert = Alert(title: Text("Unfavorite Failed"),
-                               message: Text("Failed to unfavorite this user, please try again"),
-                               primaryButton: .default(
-                                 Text("Try Again"),
-                               action: {
-                                 self.unfavorite(favoriteeId: favoriteeId)
-                               }
-                               ),
-                               secondaryButton: .default(
-                                 Text("Close"),
-                                 action: {
-                                   self.showAlert = false
-                                   self.alert = nil
-                                 }
-                           )
-                         )
-            self.showAlert = true
+        case .success:
+          if let _: APIData<Favorite> = response.value {
+            self.refreshCurrentUser()
+          }
+        case .failure:
+          self.alert = Alert(title: Text("Unfavorite Failed"),
+                             message: Text("Failed to unfavorite this user, please try again"),
+                             primaryButton: .default(
+                              Text("Try Again"),
+                              action: {
+                                self.unfavorite(favoriteeId: favoriteeId)
+                              }
+                             ),
+                             secondaryButton: .default(
+                              Text("Close"),
+                              action: {
+                                self.showAlert = false
+                                self.alert = nil
+                              }
+                             )
+          )
+          self.showAlert = true
         }
-    }
+      }
   }
   
   //
@@ -413,33 +413,33 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/players", method: .post, parameters: params, headers: self.headers!)
       .validate()
       .responseDecodable {
-      ( response: AFDataResponse<APIData<Player>>) in
+        ( response: AFDataResponse<APIData<Player>>) in
         switch response.result {
-          case .success:
-            if let value: APIData<Player> = response.value {
-              self.getGame(id: self.game!.id)
-              player = value.data
-            }
-          case .failure:
-            self.alert = Alert(title: Text("Create Player Failed"),
-                               message: Text("Failed to add this user to this game, please try again"),
-                               primaryButton: .default(
-                                 Text("Try Again"),
-                               action: {
-                                 self.createPlayer(status: status, userId: userId, gameId: gameId)
-                               }
-                               ),
-                               secondaryButton: .default(
-                                 Text("Close"),
-                                 action: {
-                                   self.showAlert = false
-                                   self.alert = nil
-                                 }
-                           )
-                         )
-            self.showAlert = true
+        case .success:
+          if let value: APIData<Player> = response.value {
+            self.getGame(id: self.game!.id)
+            player = value.data
+          }
+        case .failure:
+          self.alert = Alert(title: Text("Create Player Failed"),
+                             message: Text("Failed to add this user to this game, please try again"),
+                             primaryButton: .default(
+                              Text("Try Again"),
+                              action: {
+                                self.createPlayer(status: status, userId: userId, gameId: gameId)
+                              }
+                             ),
+                             secondaryButton: .default(
+                              Text("Close"),
+                              action: {
+                                self.showAlert = false
+                                self.alert = nil
+                              }
+                             )
+          )
+          self.showAlert = true
         }
-    }
+      }
     return player
   }
   
@@ -469,33 +469,33 @@ class ViewModel: ObservableObject {
     AF.request("http://secure-hollows-77457.herokuapp.com/players/" + String(playerId), method: .patch, parameters: params, headers: self.headers!)
       .validate()
       .responseDecodable {
-      ( response: AFDataResponse<APIData<Player>> ) in
+        ( response: AFDataResponse<APIData<Player>> ) in
         switch response.result {
-          case .success:
-            if let _: APIData<Player> = response.value {
-              self.getGame(id: self.game!.id)
-              self.refreshCurrentUser()
-            }
-          case .failure:
-            self.alert = Alert(title: Text("Edit Player Status Failed"),
-                               message: Text("Failed to edit the status of this player, please try again"),
-                               primaryButton: .default(
-                                 Text("Try Again"),
-                               action: {
+        case .success:
+          if let _: APIData<Player> = response.value {
+            self.getGame(id: self.game!.id)
+            self.refreshCurrentUser()
+          }
+        case .failure:
+          self.alert = Alert(title: Text("Edit Player Status Failed"),
+                             message: Text("Failed to edit the status of this player, please try again"),
+                             primaryButton: .default(
+                              Text("Try Again"),
+                              action: {
                                 self.editPlayerStatus(playerId: playerId, status: status)
-                               }
-                               ),
-                               secondaryButton: .default(
-                                 Text("Close"),
-                                 action: {
-                                   self.showAlert = false
-                                   self.alert = nil
-                                 }
-                           )
-                         )
-            self.showAlert = true
+                              }
+                             ),
+                             secondaryButton: .default(
+                              Text("Close"),
+                              action: {
+                                self.showAlert = false
+                                self.alert = nil
+                              }
+                             )
+          )
+          self.showAlert = true
         }
-    }
+      }
   }
   
   //
@@ -549,13 +549,13 @@ class ViewModel: ObservableObject {
   //  :param button (String) - text for the dismiss button
   func createAlert(title: String, message: String, button: String) -> Alert {
     return Alert(title: Text(title),
-          message: Text(message),
-          dismissButton: .default(
-            Text(button),
-            action: {
-              self.alert = nil
-            }
-      )
+                 message: Text(message),
+                 dismissButton: .default(
+                  Text(button),
+                  action: {
+                    self.alert = nil
+                  }
+                 )
     )
   }
 }
--- basketball-frontend/Views/GameDetailsView.swift
+++ basketball-frontend/Views/GameDetailsView.swift
@@ -135,7 +135,8 @@ struct GameDetailsView: View {
     
     .onAppear { self.viewModel.getGame(id: player.game.data.id) }
     .sheet(isPresented: $showingUsers) {
-      UsersListView(viewModel: viewModel, users: $users, status: selectedStatusList)
+      //      UsersListView(viewModel: viewModel, users: $users, status: selectedStatusList)
+      UsersListView(viewModel: viewModel, users: $users)
     }
     .actionSheet(isPresented: $showingActionSheet) {
       ActionSheet(title: Text("Change Status"), message: Text("Select a new color"), buttons: [
@@ -145,172 +146,61 @@ struct GameDetailsView: View {
         .default(Text("Not Going")) { statusChange(selectedStatus: "I'm Not Going") },
         .cancel()
       ])
-      
-      VStack {
-        VStack {
-          HStack {
-            Button(action: {
-              assignUsers(users: viewModel.invited, status: "Invited")
-            }) {
-              Text("Invited")
-                .padding()
-                .background(Color.red)
-                .foregroundColor(.black)
-                .cornerRadius(40)
-                .padding([.trailing, .leading])
-            }
-            Button(action: {
-              assignUsers(users: viewModel.going, status: "Going")
-            }) {
-              Text("Going")
-                .padding()
-                .background(Color.red)
-                .foregroundColor(.black)
-                .cornerRadius(40)
-                .padding([.trailing, .leading])
-            }
-            Button(action: {
-              assignUsers(users: viewModel.maybe, status: "Maybe")
-            }) {
-              Text("Maybe")
-                .padding()
-                .background(Color.red)
-                .foregroundColor(.black)
-                .cornerRadius(40)
-                .padding([.trailing, .leading])
-            }
-          }
-          Button(action: {
-            showingActionSheet = true
-          }) {
-            Text("Change Status")
-              .padding()
-              .frame(maxWidth: .infinity)
-              .background(Color.red)
-              .foregroundColor(.black)
-              .cornerRadius(40)
-              .padding([.trailing, .leading])
-          }
-          NavigationLink(destination: InvitingUsersView(viewModel: viewModel)) {
-            Text("Invite Friends")
-              .padding()
-              .frame(maxWidth: .infinity)
-              .background(Color.red)
-              .foregroundColor(.black)
-              .cornerRadius(40)
-              .padding([.trailing, .leading])
-          }
-          
-          HStack {
-            Text("Name:")
-              .padding(.leading)
-            Spacer()
-            Text(player.game.data.name)
-              .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-          }              .padding([.trailing, .leading])
-          
-          
-          HStack {
-            Text("Date:")
-              .padding(.leading)
-            Spacer()
-            Text(player.game.data.onDate())
-              .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-          }              .padding([.trailing, .leading])
-          
-          HStack {
-            Text("Time:")
-              .padding(.leading)
-            Spacer()
-            Text(player.game.data.onTime())
-              .fontWeight(.bold)
-          }              .padding([.trailing, .leading])
-          
-          HStack {
-            Text("Status:")
-              .padding(.leading)
-            Spacer()
-            Text(status)
-              .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-          }              .padding([.trailing, .leading])
-          
-        }
-        .navigationBarTitle("Game Details")
-        .onAppear { self.viewModel.getGame(id: player.game.data.id) }
-        .sheet(isPresented: $showingUsers) {
-          UsersListView(viewModel: viewModel, users: $users)
-        }
-        .actionSheet(isPresented: $showingActionSheet) {
-          ActionSheet(title: Text("Change Status"), message: Text("Select a new color"), buttons: [
-            .default(Text("Invited")) { statusChange(selectedStatus: "I'm Invited") },
-            .default(Text("Maybe")) { statusChange(selectedStatus: "I'm Maybe") },
-            .default(Text("Going")) { statusChange(selectedStatus: "I'm Going") },
-            .default(Text("Not Going")) { statusChange(selectedStatus: "I'm Not Going") },
-            .cancel()
-          ])
-          //         UsersListView(users: $users, viewModel: viewModel)
-        }
-        Spacer()
-      }
-      
-      //MARK: - Player List Button Struct
-      struct PlayerListButton: View{
-        var selectedUsers: [Users]
-        var status: String
-        var image: String
-        @Binding var users: [Users]
-        @Binding var showingUsers: Bool
-        @Binding var selectedStatusList: String
-        var CR: CGFloat = 20
-        var body: some View{
-          //Button to view going players
-          Button(action: { assignUsers(users: selectedUsers, status: status)}){
-            VStack{
-              Image(systemName: image)
-                .font(.system(size: 20))
-                .frame(width:22, height: 20)
-                .padding(.bottom,2)
-              Text("\(selectedUsers.count) \(status)")
-            }
-            //Vstack modifiers
-            .padding(10)
-            .background(Color("primaryButtonColor"))
-            .foregroundColor(.black)
-            .cornerRadius(CR)
-          }
-          //button modifiers
-          .padding(10)
-        }
-        
-        func assignUsers(users: [Users], status: String) {
-          self.users = users
-          self.showingUsers = true
-          self.selectedStatusList = status
-        }
-      }
-      .actionSheet(isPresented: $showingActionSheet) {
-        ActionSheet(title: Text("Change Status"), message: Text("Select a new color"), buttons: [
-          .default(Text("Invited")) { statusChange(selectedStatus: "I'm Invited") },
-          .default(Text("Maybe")) { statusChange(selectedStatus: "I'm Maybe") },
-          .default(Text("Going")) { statusChange(selectedStatus: "I'm Going") },
-          .default(Text("Not Going")) { statusChange(selectedStatus: "I'm Not Going") },
-          .cancel()
-        ])
-      }
-      Spacer()
-    }.alert(isPresented: $viewModel.showAlert) {
-      viewModel.alert!
-      
-      //MARK: - Helper Methods
-      
-      
-      func statusChange(selectedStatus: String) {
-        print(selectedStatus)
-        viewModel.editPlayerStatus(playerId: self.player.id, status: selectedStatus)
-        self.status = selectedStatus
-      }
-      
     }
+    .alert(isPresented: $viewModel.showAlert) {
+      viewModel.alert!
+    }
+    Spacer()
+  }
+  
+  
+  //MARK: - Helper Methods
+  
+  func statusChange(selectedStatus: String) {
+    print(selectedStatus)
+    viewModel.editPlayerStatus(playerId: self.player.id, status: selectedStatus)
+    self.status = selectedStatus
+  }
+  
+  func assignUsers(users: [Users], status: String) {
+    self.users = users
+    self.showingUsers = true
+    self.selectedStatusList = status
+  }
+}
+
+//MARK: - Player List Button Struct
+struct PlayerListButton: View {
+  var selectedUsers: [Users]
+  var status: String
+  var image: String
+  @Binding var users: [Users]
+  @Binding var showingUsers: Bool
+  @Binding var selectedStatusList: String
+  var CR: CGFloat = 20
+  var body: some View{
+    //Button to view going players
+    Button(action: { assignUsers(users: selectedUsers, status: status)}){
+      VStack{
+        Image(systemName: image)
+          .font(.system(size: 20))
+          .frame(width:22, height: 20)
+          .padding(.bottom,2)
+        Text("\(selectedUsers.count) \(status)")
+      }
+      //Vstack modifiers
+      .padding(10)
+      .background(Color("primaryButtonColor"))
+      .foregroundColor(.black)
+      .cornerRadius(CR)
+    }
+    //button modifiers
+    .padding(10)
+  }
+  func assignUsers(users: [Users], status: String) {
+    self.users = users
+    self.showingUsers = true
+    self.selectedStatusList = status
   }
 }
 
